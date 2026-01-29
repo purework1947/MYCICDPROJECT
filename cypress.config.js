0680e1494d9bbd76d835c13895cbd063fea3aaa9
AhmedALi <mac@Macs-MacBook-Pro.local>
2026-01-30 01:04:43 +0500

    changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require("allure-cypress");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      require("allure-cypress/on")(on, config);
+      allureWriter(on, config);
       return config;
     },
   },
